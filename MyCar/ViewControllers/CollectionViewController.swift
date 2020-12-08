9b2b122f11b8435e7ce7d5a0f7363ace35a8e2ba
Alexander Khartanovich <khartanovichao@gmail.com>
2020-12-08 08:38:13 +0300

    Collection view

---
+++ MyCar/ViewControllers/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  MyCar
+//
+//  Created by MacBook on 7.12.20.
+//  Copyright © 2020 khartanovichao. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
